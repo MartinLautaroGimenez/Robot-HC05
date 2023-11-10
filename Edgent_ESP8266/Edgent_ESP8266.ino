3add80e1f24a5652da2543ba86d232a543515aa8
Martin Lautaro Gimenez <142127631+martinLgimenez@users.noreply.github.com>
2023-11-10 16:41:08 -0300

    Update Edgent_ESP8266.ino

--- Edgent_ESP8266/Edgent_ESP8266.ino
+++ Edgent_ESP8266/Edgent_ESP8266.ino
@@ -1,26 +1,3 @@
-/*************************************************************
-  Blynk is a platform with iOS and Android apps to control
-  ESP32, Arduino, Raspberry Pi and the likes over the Internet.
-  You can easily build mobile and web interfaces for any
-  projects by simply dragging and dropping widgets.
-
-    Downloads, docs, tutorials: https://www.blynk.io
-    Sketch generator:           https://examples.blynk.cc
-    Blynk community:            https://community.blynk.cc
-    Follow us:                  https://www.fb.com/blynkapp
-                                https://twitter.com/blynk_app
-
-  Blynk library is licensed under MIT license
- *************************************************************
-  Blynk.Edgent implements:
-  - Blynk.Inject - Dynamic WiFi credentials provisioning
-  - Blynk.Air    - Over The Air firmware updates
-  - Device state indication using a physical LED
-  - Credentials reset using a physical Button
- *************************************************************/
-
-/* Fill in information from your Blynk Template here */
-/* Read more: https://bit.ly/BlynkInject */
 #define BLYNK_TEMPLATE_ID "TMPL2F7NU1aS1"
 #define BLYNK_TEMPLATE_NAME "Auto"
 
@@ -38,17 +15,42 @@
 //#define USE_WEMOS_D1_MINI
 
 #include "BlynkEdgent.h"
+#include "LedControl.h" // Incluye la biblioteca LedControl
+LedControl lc = LedControl(D8, D3, D7, 1); // Crea el objeto LedControl
 
 int motorA_1 = D1;
 int motorA_2 = D0;
-int motorB_1 = D3;
-int motorB_2 = D4;
+int motorB_1 = D5;
+int motorB_2 = D6;
 int ENA = D2;
 int ENB = D6;
 int adelante;
 int atras;
 int derecha;
-int izquierda
+int izquierda;
+int estadomatriz = 0;
+int cfeliz;
+int ctriste;
+//Comprobar movimientos
+BLYNK_WRITE(V0){ //adelante
+  adelante = param.asInt();
+}
+BLYNK_WRITE(V1){ //atras FALLA
+  atras = param.asInt();
+  Serial.println(param.asInt());
+}
+BLYNK_WRITE(V2){ //derecha
+  derecha = param.asInt();
+}
+BLYNK_WRITE(V3){ //derecha
+  izquierda = param.asInt();
+}
+BLYNK_WRITE(V4){
+  cfeliz = param.asInt();
+}
+BLYNK_WRITE(V5){
+  ctriste = param.asInt();
+}
 void setup()
 {
   Serial.begin(115200);
@@ -59,6 +61,9 @@ void setup()
   pinMode(motorB_2, OUTPUT);
   pinMode(ENA, OUTPUT);
   pinMode(ENB, OUTPUT);
+  lc.shutdown(0, false); // Inicializa la matriz de LED
+  lc.setIntensity(0, 10); // Ajusta la intensidad del brillo (puedes modificar este valor según tus preferencias)
+  lc.clearDisplay(0); // Limpia la matriz antes de comenzar
   BlynkEdgent.begin();
 }
 
@@ -67,46 +72,128 @@ void loop() {
   if (adelante == 255) { //Adelante
     digitalWrite(motorA_1, LOW);
     digitalWrite(motorA_2, HIGH);
-    digitalWrite(motorB_1, HIGH);
-    digitalWrite(motorB_2, LOW);
-    digitalWrite(ENA, HIGH);
-    digitalWrite(ENB, HIGH);
-    Serial.println("pa lante");
-  }
-  else if (atras == 255) { //Atras
-    digitalWrite(motorA_1, LOW); //falla
-    digitalWrite(motorA_2, LOW);
     digitalWrite(motorB_1, LOW);
     digitalWrite(motorB_2, HIGH);
     digitalWrite(ENA, HIGH);
     digitalWrite(ENB, HIGH);
-    Serial.println("pa tras");
+    Serial.println("pa lante");
+    comprobarestadomatriz();
   }
-  else if (derecha == 255) { //Derecha
+  else if (atras == 255) { //Atras
     digitalWrite(motorA_1, HIGH); //falla
     digitalWrite(motorA_2, LOW);
     digitalWrite(motorB_1, HIGH);
     digitalWrite(motorB_2, LOW);
     digitalWrite(ENA, HIGH);
     digitalWrite(ENB, HIGH);
-    Serial.println("Derecha");
+    Serial.println("pa tras");
+    comprobarestadomatriz();
   }
-  else if (adelante == 0 && atras == 0 && derecha == 0) { //Parar
+  else if (derecha == 255) { //Derecha
+    digitalWrite(motorA_1, HIGH); 
+    digitalWrite(motorA_2, LOW);
+    digitalWrite(motorB_1, HIGH);
+    digitalWrite(motorB_2, LOW);
+    digitalWrite(ENA, HIGH);
+    digitalWrite(ENB, HIGH);
+    Serial.println("Derecha");
+    comprobarestadomatriz();
+    guinederecho();
+  }
+  else if (izquierda == 255) { //Izquierda
+    digitalWrite(motorA_1, LOW); 
+    digitalWrite(motorA_2, HIGH);
+    digitalWrite(motorB_1, LOW);
+    digitalWrite(motorB_2, HIGH);
+    digitalWrite(ENA, HIGH);
+    digitalWrite(ENB, HIGH);
+    Serial.println("Derecha");
+    comprobarestadomatriz();
+    guineizquierdo();
+  }
+  else if (cfeliz == 255) { 
+    carafeliz();
+  }
+  else if (ctriste == 255) { 
+    caratriste();
+  }
+  else if (adelante == 0 && atras == 0 && derecha == 0 && izquierda == 0) { //Parar
     digitalWrite(motorA_1, LOW);
     digitalWrite(motorA_2, LOW);
     digitalWrite(motorB_1, LOW);
     digitalWrite(motorB_2, LOW);
     digitalWrite(ENA, LOW);
     digitalWrite(ENB, LOW);
+    comprobarestadomatriz();
+
   }
 }
-BLYNK_WRITE(V0){ //adelante
-adelante = param.asInt();
+
+void caratriste() {
+    // Define happy como su valor
+    byte Triste[8] = {
+      0x24, 
+      0x24, 
+      0x24, 
+      0x24, 
+      0x00, 
+      0x3c, 
+      0x42, 
+      0x81,
+  };
+  lc.clearDisplay(0); // Vaciar la matris
+  for (int row = 0; row < 8; row++) {
+  lc.setRow(0, row, Triste[row]);
+  }
+  estadomatriz = 1; //Definir la variable estadomatriz como 1
 }
-BLYNK_WRITE(V1){ //atras FALLA
-atras = param.asInt();
-Serial.println(param.asInt());
+
+void carafeliz() {
+   //Define la variable pacman
+    byte Feliz[8] = {
+0x24, 
+0x24, 
+0x24, 
+0x24, 
+0x81,
+0xc3, 
+0x3c, 
+0x00,
+  };
+  lc.clearDisplay(0); // Vaciar la matris
+  for (int row = 0; row < 8; row++) {
+  lc.setRow(0, row, Feliz[row]);
+  }
+  estadomatriz = 2; //Definir la variable estadomatriz como 1
+} 
+
+void guineizquierdo(){
+  byte Guineizquierdo[8] = {
+    0x08, 0x0c, 0x06, 0xff, 0xff, 0x06, 0x0c, 0x08
+  };
+  lc.clearDisplay(0);
+  for (int row = 0; row < 8; row++) {
+  lc.setRow(0, row, Guineizquierdo[row]);
+
 }
-BLYNK_WRITE(V2){ //derecha
-derecha = param.asInt();
+estadomatriz = 0;
+}
+
+void guinederecho(){
+  byte Guinederecho[8] = {
+    0x10, 0x30, 0x60, 0xff, 0xff, 0x60, 0x30, 0x10
+  };
+  lc.clearDisplay(0);
+  for (int row = 0; row < 8; row++) {
+  lc.setRow(0, row, Guinederecho[row]);
+
+}
+estadomatriz = 0;
+}
+
+void comprobarestadomatriz() {
+    if (estadomatriz != 1 && estadomatriz != 2) {
+
+            lc.clearDisplay(0);
+    }
 }
